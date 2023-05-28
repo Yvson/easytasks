efff387bf13bb571920ef09ba78aa84b0e582a0c
Yvson Moura <yvson@yvsonmoura.com>
2023-05-28 01:08:29 -0300

    Development config changed

--- easytasks/settings/development.py
+++ easytasks/settings/development.py
@@ -5,7 +5,7 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, 'settings/.env.development'))
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = env.bool("DEBUG", True)
+DEBUG = env.bool("DEBUG", False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
